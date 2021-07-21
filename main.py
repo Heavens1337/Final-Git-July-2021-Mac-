ed3c7c61c880c9f210bfb00413ba9b26c6b4ff00
Heavens <heyitscaelam@gmail.com>
2021-07-21 11:39:36 -0230

    Reverted to a previous version and built upon that, program works much better now

--- main.py
+++ main.py
@@ -30,3 +30,16 @@ r.read_marker()
 
 marker_check = r.read_marker()
 print(marker_check)
+
+if marker_check == 1:
+    r.backward(50)
+    r.left(600)
+    r.forward(200)
+    # do scans and stuff here
+    r.backward(200)
+    r.right(600)
+    r.forward(50)
+    r.right(50)
+
+else:
+    r.right(50)
